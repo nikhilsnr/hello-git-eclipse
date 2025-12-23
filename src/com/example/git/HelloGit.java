5d418a7f7dff9d330d57bef364a442647934a4c8
nikhilreddys <nikhilreddy4560@gmail.com>
2025-12-23 15:49:11 +0530

    Trigger Jenkins Build

--- src/com/example/git/HelloGit.java
+++ src/com/example/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println("Hello Git from Eclipse!");
+        System.out.println("CI build triggered!");
     }
 }
