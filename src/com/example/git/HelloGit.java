fcbacb120ac36782c2490f1769b6be9ada6d5250
nikhilreddys <nikhilreddy4560@gmail.com>
2025-12-23 16:49:27 +0530

    Changed HelloGit

--- src/com/example/git/HelloGit.java
+++ src/com/example/git/HelloGit.java
@@ -5,6 +5,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git from Eclipse!");
         System.out.println("CI build triggered!");
+        
         System.out.println("Webhook Added");
     }
 }
